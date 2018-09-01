d14d8bba817c6bffd36fc1d02025e1b8b9c43f95
Joerg Rossdeutscher <j.rossdeutscher@superreal.de>
2018-09-01 08:46:20 +0200

    FEAT: Add real configuration

---
+++ config_DIST.inc.php
@@ -0,0 +1,42 @@
+<?php
+
+$this->filePathToThings = __DIR__ . '/../Ereignisse2';
+$this->filePathToCache = __DIR__ . '/../Cache/Ereignisse2';
+$this->filePathToRepo = __DIR__ . '/../Ereignisse2.repo';
+
+#Lowercase!
+$this->fileExtensionsOfBasicImages = array('jpg', 'jpeg');
+$this->fileExtensionsOfRawImages = array('dng', 'raw');
+$this->fileExtensionsOfImages;
+
+$this->fileExtensionsOfBasicMovies = array('mp4');
+$this->fileExtensionsOfProprietaryMovies = array('mov', 'avi', '3gp');
+$this->fileExtensionsOfMovies;
+
+$this->fileExtensionsOfThings = array_merge(
+    $this->fileExtensionsOfBasicImages,
+    $this->fileExtensionsOfRawImages,
+    $this->fileExtensionsOfBasicMovies,
+    $this->fileExtensionsOfProprietaryMovies
+);
+
+$this->fileExtensionsOfMovies = array_merge(
+    $this->fileExtensionsOfBasicMovies,
+    $this->fileExtensionsOfProprietaryMovies
+);
+
+$this->fileExtensionsOfImages = array_merge(
+    $this->fileExtensionsOfBasicImages,
+    $this->fileExtensionsOfRawImages
+);
+
+$this->derivedFiles = array(
+    'thumbnail' => array(
+        'width' => 200,
+        'height' => 150,
+    ),
+    'poster' => array(
+        'width' => 1200,
+        'height' => 900,
+    ),
+);
