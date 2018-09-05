87388554881d6ad7a96b0a2e4fcfca9d4d5cf372
Joerg Rossdeutscher <j.rossdeutscher@superreal.de>
2018-09-05 21:16:51 +0200

    FEAT: Add security controller

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Class SecurityController
+ *
+ * This Software is the property of superReal and is protected
+ * by copyright law - it is NOT Freeware.
+ *
+ * Any unauthorized use of this software without a valid license key
+ * is a violation of the license agreement and will be prosecuted by
+ * civil and criminal law.
+ *
+ * @link      http://www.superreal.de
+ * @package App\Controller
+ * @copyright (C) superReal 2018
+ * @author    Joerg Rossdeutscher <j.rossdeutscher _AT_ superreal.de>
+ */
+
+namespace App\Controller;
+
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class SecurityController extends AbstractController
+{
+
+    /**
+     * @Route("/login", name="login")
+     */
+
+    public function login(AuthenticationUtils $authenticationUtils)
+    {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', array(
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ));
+    }
+}
